035eb3e1da111553305099119d77f4340f5a1aaf
vgd15 <deise171315@gmail.com>
2023-04-17 22:06:42 -0300

    primeira versão do meu projeto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function Event() {
+ if (document.getElementById("nome").value != "" && document.getElementById("email").value != "") {
+  alert("Prontinho");
+} else {
+  alert("Por favor, preencha os campos nome e email");
+}
+}
